34e43344fa75a9ced2d315b436050af87e741f1d
Marek Gudalewicz <marek.gudalewicz@localhost>
2021-04-27 19:10:47 +0200

    Infix to prefic is a cleaned set of  fnuctions. Still not a class

--- WorkingDir/infixToPrefix.js
+++ WorkingDir/infixToPrefix.js
@@ -25,67 +25,101 @@ function infix2prefix(inputList) {
 	}
 
 	function throwSyntaxErrorCloseBracketMissing(){
-		throw new SyntaxError(`${this.constructor.name}: ')' missing`)
+		if (!isOperator(stackPeep() || stackPeep() === "$")){
+			console.log('SyntaxError )')
+			throw new SyntaxError(`${this.constructor.name}: ')' missing`)
+		}
 	}
 
 
 	function processEachInputItem(currentItem, i, listOfItems){
-		// currentItem = currentItem;
-		console.log(currentItem)
-		console.log(outList)
-		stackCurrent = stackPeep();
-		console.log(stack)
-		if (isNaN(Number(currentItem)) === false) {      // 1) if item is a number add it to the begining of the list
-			outList.unshift(Number(currentItem));
-			console.log('is numbner')
-		} else if (currentItem === ")") {
-			stack.push(currentItem);
-			console.log('is )')
-		} else if (isOperator(currentItem)) {  // 3) if an item is an operator then pop from stack and push them to outList as long as they have prio higher or equal to an item
-			console.log(stackPeep()); console.log(getSymbolPriority(stackPeep())); console.log(getSymbolPriority(currentItem))
-			console.log('Current Item', currentItem)
-			console.log('stack peep', stackPeep())
-			while (isOperator(stackPeep()) && (getSymbolPriority(stackPeep()) >= getSymbolPriority(currentItem))) {
-				let statckPop = stack.pop()
-				outList.unshift(stack.pop());
-				console.log(outList)
-			} // while
-		stack.push(currentItem);  // 4) push item (operator) to the stack
-		} else if (currentItem === "(") {
-			while (stackPeep() !== ")") {
-				if (stackPeep() === ")") {console.log(" stackPeep is ) ");}
-				console.log("stack here is : " + String(stack));
-				console.log(stackPeep())
-				if (!isOperator(stackPeep()) || stackPeep() === "$") {throwSyntaxErrorCloseBracketMissing()}  // if stack is empty "$" or there is anything that is not an operator or ")" report bad syntax
-				if ( stackPeep() != ')')
-					{
-						outList.unshift(stack.pop());  // 5) if an item is an "(" then remove from stack all operators till ")" is encountered
-					} else {
-						stack.pop();
-					}
+		let isANumber = function(val) { return !isNaN(Number(val))}
+		let pushItemToBeginingOfList = function(item) {outList.unshift(item)}
 
-				// Here '$' is added to output list !!! 
-		}
-			stack.pop();   // 
-		} // else if
-		if ((i === listOfItems.length - 1) && stack.length > 1) {  // 6) If there was no () at beging or end of expression empty stack of all operators
-			while (stack.length > 1) {   // stack can be length 1 because of special value $
-				outList.unshift(stack.pop());
+		let repeatCallbackAsLongAsConditionIsMet = function(callback, condition, onErrorBeforeEachCall = () => {}){
+			console.log(condition())
+			while (condition()) {
+				console.log(stackPeep())
+				onErrorBeforeEachCall();
+				callback()
 			}
-		}  // if i===0 && stack.length > 1 
-		console.log("End of iteration " + i + " Stack is: " + String(stack));
+		}
+		let popFromStackUnshiftToList = function(){
+			outList.unshift(stack.pop())
+		}
+		let isStackTopOperatorsPrioGreaterEqualThenCurrentElementsPrio = function(){
+			console.log(stackPeep())
+			console.log(getSymbolPriority(stackPeep()))
+			console.log(getSymbolPriority(currentItem))
+			return isOperator(stackPeep()) && (getSymbolPriority(stackPeep()) >= getSymbolPriority(currentItem))
+		}
+
+		console.log(currentItem)
+
+		if (isANumber(currentItem)) {
+			console.log('Is a numner ' + currentItem)
+			pushItemToBeginingOfList(currentItem)
+		} else if (currentItem === ")") {
+			stack.push(currentItem)
+		} else if (isOperator(currentItem)){
+			console.log('Is operator' + currentItem)
+			repeatCallbackAsLongAsConditionIsMet(popFromStackUnshiftToList, isStackTopOperatorsPrioGreaterEqualThenCurrentElementsPrio)
+			stack.push(currentItem)
+		} else if (currentItem == '(') {
+			repeatCallbackAsLongAsConditionIsMet(popFromStackUnshiftToList, ()=>{return stackPeep() != ')'}, throwSyntaxErrorCloseBracketMissing)
+			stack.pop();
+		}
+		if ((i == listOfItems.length - 1) && (stack.length > 1)) {
+			repeatCallbackAsLongAsConditionIsMet(popFromStackUnshiftToList(), () => {stack.length > 1})
+		}
+		console.log(stack.toString())
+		console.log(stackPeep())
+		console.log(outList.toString())
+		// return outList;
+
+
+
+		// if (isNaN(Number(currentItem)) === false) {      // 1) if item is a number add it to the begining of the list
+		// 	outList.unshift(Number(currentItem));
+		// } else if (currentItem === ")") {
+		// 	stack.push(currentItem);
+		// } else if (isOperator(currentItem)) {  // 3) if an item is an operator then pop from stack and push them to outList as long as they have prio higher or equal to an item
+		// 	while (isOperator(stackPeep()) && (getSymbolPriority(stackPeep()) >= getSymbolPriority(currentItem))) {
+		// 		let statckPop = stack.pop()
+		// 		outList.unshift(stack.pop());
+		// 	} // while
+		// stack.push(currentItem);  // 4) push item (operator) to the stack
+		// } else if (currentItem === "(") {
+		// 	while (stackPeep() !== ")") {
+		// 		if (!isOperator(stackPeep()) || stackPeep() === "$") {throwSyntaxErrorCloseBracketMissing()}  // if stack is empty "$" or there is anything that is not an operator or ")" report bad syntax
+		// 		if ( stackPeep() != ')')
+		// 			{
+		// 				outList.unshift(stack.pop());  // 5) if an item is an "(" then remove from stack all operators till ")" is encountered
+		// 			} else {
+		// 				stack.pop();
+		// 			}
+
+		// 		// Here '$' is added to output list !!! 
+		// }
+		// 	stack.pop();   // 
+		// } // else if
+		// if ((i === listOfItems.length - 1) && stack.length > 1) {  // 6) If there was no () at beging or end of expression empty stack of all operators
+		// 	while (stack.length > 1) {   // stack can be length 1 because of special value $
+		// 		outList.unshift(stack.pop());
+		// 	}
+		// }  // if i===0 && stack.length > 1 
 	}
 	
 	try {
+		console.log('processEach should run')
 		inputList.reverse().forEach(processEachInputItem)
 		// for (var i=(inputList.length - 1); i>=0; i--) {    // 0) for each element from right to left do:
-
-		// }   // for i
 		console.log(outList)
+		// }   // for i
 		return outList;
 	} // try
 	catch(err) {
-		console.trace(err)
+	
 		return err;
 		}
 	
