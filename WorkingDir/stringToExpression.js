f2336ff16c74b089ee793966ce03de162af6412f
Marek Gudalewicz <marek.gudalewicz@localhost>
2021-04-28 01:58:21 +0200

    Function refabricated to be tested

--- WorkingDir/stringToExpression.js
+++ WorkingDir/stringToExpression.js
@@ -1,44 +1,151 @@
-function stringToExpression() {
-	//var history; // an object containing pares: strInput, strOutput
-	return convert = function(strInput){
-		console.log(" 2.3.4 is " + isNaN(Number('2.3.2')));
-		if (strInput.length > 0) {
-			var numberBeingConverted = "",
-		    	syntaxError = false; // was a syntax error detected?
-			var result = [];
-			console.log("stringToExpression.LengthOfString = " + strInput.length);
-			for (var i = 0; i < strInput.length; i++) {
-				if (isNaN(Number(strInput[i])) && (strInput[i] !== ".")) {
-					if (numberBeingConverted !== "") {
-						try {   // cannot convert - syntax went wrong -> eg two dots in one expresion
-							let currentConversion = Number(String(numberBeingConverted));
-							if (isNaN(currentConversion)) { throw "SyntaxErr!";}
-								else {
-							result.push(currentConversion)
-							result.push(strInput[i]);
-							numberBeingConverted = "";
-							}
-						}
-						catch(err) { syntaxError = true;}
-					}  // if (numberBeingConverted ...
-					else {
-						result.push(strInput[i]);
-					}
-				}  // if (isNaN)Number(strInput[i] ...
-				else {  // strInput[i]  is a number
-					numberBeingConverted += strInput[i];	
-				} // else  -> so strInput[i] is a number			
-			} // for strInput
-			if (numberBeingConverted !== "") {
-				try {if (isNaN(numberBeingConverted)) {throw "SyntacErr";} 
-					else {result.push(numberBeingConverted);}  // if last element to be converted is a number it will be converted here
-					}  //try
-			    catch(err) {syntaxError = true;}
-		   }  // if mumberBeingConverted!== ...
-			if (syntaxError) {return "SyntaxError";} else { return result; }
-		} else  { // if strInput.lenght > 0
-			return "";
-		}			
+function stringToExpression(strInput) {
+
+		if (strInput.length = 0) { return ""}
+
+		let numberBeingConverted = "";
+		let syntaxError = false; 
+		let result = [];
+		let numberConverter = null;
+
+		let allowedCharacters = '().+-/*'
+		let plusMinusUnipolarAllowedAfter = '(+-'
+
+
+
+		let isCharacterADigit = function(character) {
+			return !isNaN(parseInt(String(character)))
 		}
-		//return convert(s);
-} //strToExpresion
+
+
+		let isCharacterIn = function(charactersChain, character){
+			return charactersChain.indexOf(character) != -1
+		}
+
+		let isCharacterSupported = function(character) {
+			return isCharacterIn(allowedCharacters, character)
+		}
+
+
+		let ifCharacterIsIllegalCharacterThrowError = function(character) {
+			let conditionArrayForSingleCharacter = [
+				(character) => {return isNaN(parseInt(character))},
+				(character) => {return !isCharacterSupported(character)}
+			]
+			let characterIsIllegal = conditionArrayForSingleCharacter.reduce((acc, condition, index) => {
+				if (index == 1) {acc = acc(character);}
+				return acc && condition(character)
+			})
+			if (characterIsIllegal) {throw new SyntaxError(`${this.constructor.name}: character ${character} is not supported`)}
+		}
+
+
+		let processSingleCharacter = function(character, index, strInput) {
+
+			let getLastProcessedCharacter = function(){
+				return (index == 0) ? null : strInput[index - 1]
+			}	
+			let wasLastCharacterIn = function(charactersChain) {
+				return isCharacterIn(charactersChain, getLastProcessedCharacter())
+			}
+
+			let isThisFirstProcessedCharacter = function(){
+				return index == 0 ? true : false;
+			}
+
+			ifCharacterIsIllegalCharacterThrowError(character)
+			// result.push(character)
+
+			if (isCharacterADigit(character)) {
+				nextNumberConversionStep()
+				return null
+			} 
+			if (isCharacterIn('+-')){
+				if (wasLastCharacterIn('(-+') || isThisFirstProcessedCharacter()) {
+					nextNumberConversionStep()
+					return null
+				}
+			}
+			if (isCharacterIn('+-*/')){
+				if (isCharacterADigit(getLastProcessedCharacter) || isThisFirstProcessedCharacter() || isCharacterIn(')')){
+					finalizePreviousNumberConversion();
+					addOperatorToExpression()	
+					return null
+				}
+			}
+
+			throw new SyntaxError(`${this.constructor.name}: last character was ${getLastProcessedCharacter()}, 
+								   current is ${character}. This is not supported combination`
+			)
+		}
+
+
+		function nextNumberConversionStep(character) {
+			getNumberConverter().nextConversionStep(character)
+		}
+
+
+		function finalizePreviousNumberConversion() {
+			result.push(getCurrentNumberConverter.getProcessedSoFar())
+			getNumberConverter().resetConverter()
+		}
+
+		function getNumberConverter(){
+			if (numberConverter == null) {numberConverter = new numberConverter()} 
+			return numberConverter;
+		}
+	}
+
+	class numberConverter{
+		constructor(){
+			this.convertedSoFar = '';
+			this.wasThereADot = false;
+			this.minusFlag = false;
+		}
+		nextConversionStep(character) {
+			if (!this.isCharacterADigit(character) && !this.isCharacterIn('.+-')) this.throwSyntaxError()
+			if (character == '.') {
+				if (this.wasThereADot) this.throwSyntaxError();
+				this.wasThereADot = true;
+			}
+			if (this.wasThereAnyDigitSoFar()){
+				if (this.isCharacterIn('+-')){
+					this.throwSyntaxError()
+				} 
+			} else {
+				if (character == '-') {
+					this.toggleMinusSign()
+					return null
+				}
+				if (character == '+'){
+					return null
+				}	
+			}
+			this.convertedSoFar = this.convertedSoFar + character
+		}
+		getProcessedSoFar(){
+			return parseFloat(this.convertedSoFar)
+		}
+		resetConverter(){
+			this.convertedSoFar = '';
+			this.wasThereADot = false;
+			this.minusFlag = false
+		}
+
+		isCharacterADigit(character) {
+			return !isNaN(parseInt(character))
+		}
+		isCharacterIn = function(charactersChain, character){
+			return charactersChain.indexOf(character) != -1
+		}
+		wasThereAnyDigitSoFar() {
+			return this.convertedSoFar != '' ? true : false;
+		}
+		throwSyntaxError() {
+			throw SyntaxError(`${this.constructor.name}: error in syntax: processed so far: ${this.convertedSoFar}, 
+				${this.wasThereADot ? 'there was already a dot' : 'there was not a dot yet'} currently processed character: ${character} `
+			)
+		}
+		toggleMinusSign() {
+			this.minusFlag = !this.minusFlag;
+		}
+	}
